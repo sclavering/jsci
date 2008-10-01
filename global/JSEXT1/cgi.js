c04cac85f44f63c74a9b666a4841f5be2172b90c
Stephen Clavering <stephen@clavering.me.uk>
2008-10-01 21:41:44 +0000

    cgi.js: reindent and reformat the run() method, and update its documentation to reflect that it's cgi.run(), not cgi(); and that all the magic handling of .js vs .jsx files has been removed

--- global/JSEXT1/cgi.js
+++ global/JSEXT1/cgi.js
@@ -1,48 +1,24 @@
 ({
-/*
-         cgi([refresh=false])
+  /* cgi.run([refresh=false])
 
      Interprets the environment variables given in
      _environment_ according to the CGI/1.1 specification:
      <http://hoohoo.ncsa.uiuc.edu/cgi/env.html>.
 
      The CGI standard treats some HTTP headers differently
-     from others. That distinction is reversed by the _cgi_
+     from others. That distinction is reversed by the _run_
      function. All headers are placed in the
      requestHeaders property of the created context object.
 
      This function uses the web server's url-to-path translation.
-     The behaviour of _cgi_ depends on the filename
-     extension, query string and the HTTP method:
 
-     <table>
-       <tr><td colspan='2' rowspan='3'> </td><th colspan='3'>HTTP method</th></tr>
-       <tr><th colspan='2'>GET</th><th rowspan='2'>POST</th></tr>
-       <tr><th>without query string</th><th>with query string</th></tr>
-       <tr><th rowspan='3'>Filename extension</th><th>.js</th>
-         <td>Send file</td><td colspan='2'>Include dependencies</td></tr>
-       <tr><th>.jsx</th>
-         <td>Send file</td><td colspan='2'>Execute file</td></tr>
-       <tr><th>other</th>
-         <td colspan='3'>Send file</td></tr>
-     </table>
-
-     JSEXT is usually not invoked for other files than _.js_ or _.jsx_,
-     but if it is, it will simply send the files, setting the
-     content-type according to the filename extension and the
-     mime types defined in [[$curdir.mime.type]].
-
-     JavaScript files, both _.js_ and _.jsx_ have mime type
-     "application/javascript".
-
-     ### Arguments ###
-
-     * _refresh_: Boolean. If true, _cgi_ will attempt to reload
-       ActiveDirectory objects that have been changed since they
-       were last loaded.
+     It should only be used for _.jsx_ files, which should contain a single
+     anonymous javascript function.  The function will be called with no
+     arguments, but with _this_ set to a context object with the following
+     properties:
 
      ### Context object ###
-    
+
      When _cgi_ calls a _.jsx_ function, it passes a _context object_
      as _this_, which contains the following properties:
 
@@ -59,138 +35,120 @@
      * _POST_data_: the decoded data from a POST request, if any
      * _cookie_data_: the request cookies, as a name–value mapping
 
-     For details about the execution context of _.jsx_ files,
-     see [[How to write a JSEXT webpage]].
+     ### Arguments ###
 
-     For details about the inclusion of dependencies of _.js_ files,
-     see [[Running JSEXT programs in a browser]].
+     * _refresh_: Boolean. If true, _cgi_ will attempt to reload
+       ActiveDirectory objects that have been changed since they
+       were last loaded.
+  */
+  run: function(refresh) {
+    var sendcookies;
 
-    */
-run:
-(function() {
+    stdout = new http.FlashWriter(stdout, function(stream) {
+      stdin.close();
+      if(cx.responseHeaders.location) cx.responseLine = "302 Found";
+      if(cx.responseLine) stream.write("Status: " + cx.responseLine + "\r\n");
+      sendcookies(stream);
+      mime.writeHeaders(stream, cx.responseHeaders);
+      delete cx.responseHeaders;
+    });
 
-var hostDirCache={};
-var clientDirCache={};
- 
-return function (refresh) {
-  var sendcookies;
+    try {
+      var cx = {};
+      var headers = {};
+      for(var i in environment) {
+        if(i.substr(0,5) != "HTTP_") continue;
+        var mangled = i.substr(5).toLowerCase().replace(/_./g, function(a) {
+            return a.toUpperCase().substr(1);
+          });
+        headers[mangled] = environment[i];
+      }
 
-  stdout = new http.FlashWriter(stdout, function(stream) {
-    stdin.close();
-    if(cx.responseHeaders.location) cx.responseLine="302 Found";
-    if(cx.responseLine) stream.write("Status: "+cx.responseLine+"\r\n");
-    sendcookies(stream);
-    mime.writeHeaders(stream, cx.responseHeaders);
-    delete cx.responseHeaders;
-  });
-  
-  try {
+      if(environment.CONTENT_TYPE) headers.contentType = environment.CONTENT_TYPE;
+      if(environment.CONTENT_LENGTH) headers.contentLength = environment.CONTENT_LENGTH;
+      cx.requestHeaders = headers;
+      cx.remoteAddress = environment.REMOTE_ADDR;
+      cx.method = environment.REQUEST_METHOD;
+      cx.requestURL = "http://" + (cx.requestHeaders.host || '') + environment.REQUEST_URI;
+      // these are currently set in execScript.js
+      cx.POST_data = null;
+      cx.GET_data = null
+      cx.cookie_data = null;
 
-  var cx={};
-  var headers={};
-  for (var i in environment) {
-    if (i.substr(0,5)=="HTTP_")
-      headers[i.substr(5).
-	      toLowerCase().
-	      replace(/_./g,
-		      function(a) { return a.toUpperCase().substr(1); }
-		      )]=environment[i];
-  }
+      // Set a default content type.
+      cx.responseHeaders = { contentType: 'text/html' };
 
-  if(environment.CONTENT_TYPE) headers.contentType = environment.CONTENT_TYPE;
-  if(environment.CONTENT_LENGTH) headers.contentLength = environment.CONTENT_LENGTH;
-  cx.requestHeaders = headers;
-  cx.remoteAddress = environment.REMOTE_ADDR;
-  cx.method = environment.REQUEST_METHOD;
-  cx.requestURL = "http://" + (cx.requestHeaders.host || '') + environment.REQUEST_URI;
-  // these are currently set in execScript.js
-  cx.POST_data = null;
-  cx.GET_data = null
-  cx.cookie_data = null;
-  
-  // Set a default content type.
-  cx.responseHeaders = { contentType: 'text/html' };
+      sendcookies = http.Cookie.bake.call(cx);
 
+      var filename=environment.PATH_TRANSLATED || environment.SCRIPT_FILENAME;
+      var path = $curdir.path(filename);
+      var onlyFilename = $curdir.filename(filename);
+      var urlpath = environment.PATH_INFO || environment.SCRIPT_NAME;
+      var pathParts = path.split(JSEXT_config.sep);
+      var urlParts = cx.requestURL.split("/");
+      urlParts.pop();
 
-  sendcookies=http.Cookie.bake.call(cx);
+      var i;
+      for(i = 1; i <= urlParts.length; i++)
+        if(pathParts[pathParts.length-i] != urlParts[urlParts.length-i])
+          break;
 
-  var filename=environment.PATH_TRANSLATED || environment.SCRIPT_FILENAME;
-  var path=$curdir.path(filename);
-  var onlyFilename=$curdir.filename(filename);
-  var urlpath=environment.PATH_INFO || environment.SCRIPT_NAME;
-  var pathParts=path.split(JSEXT_config.sep);
-  var urlParts=cx.requestURL.split("/");
-  urlParts.pop();
+      var root = pathParts.slice(0, pathParts.length - i + 1).join(JSEXT_config.sep);
+      var rooturl = urlParts.slice(0, urlParts.length - i + 1).join("/");
+      if(rooturl != "/") rooturl += "/";
 
-  var i;
-  for (i=1; i<=urlParts.length; i++)
-    if (pathParts[pathParts.length-i] != urlParts[urlParts.length-i])
-      break;
+      pathParts.push(onlyFilename);
+      var relFilename = JSEXT_config.sep+pathParts.slice(pathParts.length - i).join(JSEXT_config.sep);
 
-  var root=pathParts.slice(0,pathParts.length-i+1).join(JSEXT_config.sep);
-  var rooturl=urlParts.slice(0,urlParts.length-i+1).join("/");
-  if (rooturl!="/")
-    rooturl+="/";
+      var extension = relFilename.match(/\.([^.]*)$/);
+      if(extension) extension = extension[1];
 
-  pathParts.push(onlyFilename);
-  var relFilename=JSEXT_config.sep+pathParts.slice(pathParts.length-i).join(JSEXT_config.sep);
+      if(refresh) {
+        var global = (function() { return this; })();
+        this._hostDirCache = {};
+        this._clientDirCache = {};
+        global.$checkdates();
+        js['export'].global.$checkdates();
+      }
 
-  var extension=relFilename.match(/\.([^.]*)$/);
-  if (extension)
-    extension=extension[1];
+      var hostdir = this._hostDirCache[rooturl];
+      if(!hostdir) {
+        hostdir = {};
+        ActiveDirectory.call(hostdir, root);
+        this._hostDirCache[rooturl] = hostdir;
+      }
 
-  if (refresh) {
-    var global=(function(){return this;})();
-    hostDirCache={};
-    clientDirCache={};
-    global.$checkdates();
-    js['export'].global.$checkdates();
-  }
+      var clientdir = this._clientDirCache[rooturl];
+      if(!clientdir) {
+        clientdir = {};
+        ActiveDirectory.call(clientdir, root, js['export'].handlers, js['export'].platform);
+        this._clientDirCache[rooturl] = clientdir;
+        var relroot = $curdir.path(urlpath.replace(/\//g, JSEXT_config.sep));
+        relroot = relroot.split(JSEXT_config.sep);
+        relroot = relroot.slice(0, relroot.length - i + 1).join("/");
+        if(relroot!="/") relroot += "/";
+        clientdir.$url = relroot;
+      }
 
-  var hostdir=hostDirCache[rooturl];
-  if (!hostdir) {
-    hostdir={};
-    ActiveDirectory.call(hostdir, root);
-    hostDirCache[rooturl]=hostdir;
-  }
-
-  var clientdir=clientDirCache[rooturl];
-  if (!clientdir) {
-    clientdir={};
-    ActiveDirectory.call(clientdir, root, js['export'].handlers, js['export'].platform);
-    clientDirCache[rooturl]=clientdir;
-    var relroot = $curdir.path(urlpath.replace(/\//g, JSEXT_config.sep));
-    relroot=relroot.split(JSEXT_config.sep);
-    relroot=relroot.slice(0,relroot.length-i+1).join("/");
-    if (relroot!="/")
-      relroot+="/";
-
-    clientdir.$url=relroot;
-  }
-
-  cx.clientdir=clientdir;
-  cx.hostdir=hostdir;
-  cx.filename="/"+pathParts.slice(pathParts.length-i).join(JSEXT_config.sep);
-  //  cx.responseLine="200 OK";
-  
-  // 2008-09-28-22-26 steve: entirely removed the dispatching based on file extension.
-  http.execScript.call(cx, refresh);
-
-  } catch(x) {
-    print(' <br/>\n');
-    if (x.fileName && x.lineNumber)
-      print('Line '+x.lineNumber+' in '+x.fileName+':');
-    print(x+' <br/>\n');
-    if (x.stack) {
-      var stack=x.stack.split('\n');
-      if (stack.length>6)
-	print(stack.slice(1,stack.length-5).join(' <br/>\n')+' <br/>\n');
+      cx.clientdir = clientdir;
+      cx.hostdir = hostdir;
+      cx.filename = "/" + pathParts.slice(pathParts.length - i).join(JSEXT_config.sep);
+      //  cx.responseLine="200 OK";
+      
+      // 2008-09-28-22-26 steve: entirely removed the dispatching based on file extension.
+      http.execScript.call(cx, refresh);
+    
+    } catch(x) {
+      print(' <br/>\n');
+      if(x.fileName && x.lineNumber) print('Line ' + x.lineNumber + ' in ' + x.fileName + ':');
+      print(x + ' <br/>\n');
+      if(x.stack) {
+        var stack = x.stack.split('\n');
+        if(stack.length > 6) print(stack.slice(1, stack.length - 5).join(' <br/>\n') + ' <br/>\n');
+      }
     }
-  }
-
-  stdout.close();
-}
-
-})()
-
+    stdout.close();
+  },
+  _hostDirCache: {},
+  _clientDirCache: {},
 })
