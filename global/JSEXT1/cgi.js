f9a53e0468b9c8a382fbe5ea9dd06ce48f2b33be
Stephen Clavering <stephen@clavering.me.uk>
2008-10-13 17:36:12 +0000

    CGI: merge ._execScript() into .run()

--- global/JSEXT1/cgi.js
+++ global/JSEXT1/cgi.js
@@ -109,7 +109,19 @@ CGI.prototype = {
       cx.filename = "/" + pathParts.slice(pathParts.length - i).join(JSEXT_config.sep);
       //  cx.responseLine="200 OK";
 
-    this._execScript();
+    var filename = this.filename;
+
+    var pathparts = filename.split(JSEXT_config.sep);
+    var curdir = this.hostdir;
+    for(var i = 1; i < pathparts.length - 1; i++) curdir = curdir[pathparts[i]];
+
+    var onlyFilename = JSEXT1.filename(filename);
+
+    const func = load.call(curdir, curdir.$path + JSEXT_config.sep + onlyFilename);
+    if(typeof func != "function") return;
+
+    this._exec_page_function(func);
+
     stdout.close();
   },
   _hostDirCache: {},
@@ -128,70 +140,6 @@ CGI.prototype = {
     if(environment.CONTENT_TYPE) headers.contentType = environment.CONTENT_TYPE;
     if(environment.CONTENT_LENGTH) headers.contentLength = environment.CONTENT_LENGTH;
     return headers;
-  },
-
-
-  /*
-     The script in _filename_ can access $parent.$parent... up to
-     the level of _hostdir_, but not above it.
-
-     Executes a script file, usually with a _.jsx_ extension.
-     This is a three-step procedure:
-
-     1. The script file is interpreted
-     2. The script is executed.
-     3. If the script evaluates to a function, i.e. if the last
-        expression in the file is a function, that function will
-        be called.
-
-     Phases 1 and 2 are cached, so that each script
-     file is only interpreted and executed once during the lifetime of the
-     JavaScript session. However, the modification time is checked
-     for each call to execScript, and if the script file is modified,
-     it will be reinterpreted. Phase 3 is repeated
-     for each call to execScript.
-
-     In phase 2, the _this_ object, and the first object
-     in the scope chain, is an ActiveDirectory object representing
-     the directory in which the script file is stored. This means
-     that a script can call functions in the same directory as
-     itself without specifying any path.
-
-     In phase 3, the _this_ object passed to the function
-     is an object containing the following properties:
-
-     * _responseHeaders_: An object containing response headers to be sent.
-
-     If the Content-Type is neither "text/JSON", "application/x-www-form-urlencoded"
-     or "multipart/form-data", the body of the request is not read by execScript.
-     In that case, the script can read from [[stdin]] in phase 3.
-
-     If the function takes no arguments, the body of the request is also not read by execScript.
-     In that case, the script can read from [[stdin]] in phase 3.
-
-     Any output to _stdout_ in phase 2 or 3 (usually using print)
-     is sent to the client. The _responseHeaders_
-     are automatically sent the first time anything is written to stdout.
-     The default Content-Type is text/html. Note that the connection is
-     buffered, so you may need to use stdout.flush() in order to actually
-     send data over the network connection.
-
-     Any exceptions produced during the execution of a script are
-     caught and sent as HTML text to the client.
-  */
-  _execScript: function() {
-      var filename = this.filename;
-
-      var pathparts = filename.split(JSEXT_config.sep);
-      var curdir = this.hostdir;
-      for(var i = 1; i < pathparts.length - 1; i++) curdir = curdir[pathparts[i]];
-
-      var onlyFilename = JSEXT1.filename(filename);
-
-      const func = load.call(curdir, curdir.$path + JSEXT_config.sep + onlyFilename);
-      if(typeof func != "function") return;
-
-      this._exec_page_function(func);
   },
 
 
