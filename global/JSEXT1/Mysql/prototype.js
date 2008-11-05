713f7c93a29cab8cc47f4d409aa06614395262e5
Stephen Clavering <stephen@clavering.me.uk>
2008-11-05 05:43:30 +0000

    JSEXT1.Mysql: remove .prepare(), since unbuffered queries (fetching one result row at a time) make for horrible code

--- global/JSEXT1/Mysql/prototype.js
+++ global/JSEXT1/Mysql/prototype.js
@@ -39,40 +39,6 @@
 
 
   /*
-  db.prepare(qry, [param, param, ...])
-
-  Performs an unbuffered query, i.e. one where the
-  whole result is not stored in memory at once.
-  Return a [[Connection]] object, creating a new
-  one if necessary. Parameter substitution works the same
-  as with [[$curdir.query]].
-
-      var res = db.prepare(qry);
-      var row;
-      while (row = res.row()) {
-        ...
-      }
-      res.free();
-
-  See also [[Connection.prototype.free]] and
-  [[Connection.prototype.row]].
-  */
-  prepare: function() {
-    var conn = this.getConnection();
-    conn.exec.apply(conn, arguments);
-    conn.result = lib.mysql_use_result(conn.mysql);
-    if (!conn.result) {
-      conn.free();
-      conn.throwError();
-    }
-    conn.rowNumber = 0;
-    conn.result.finalize = lib.mysql_free_result;
-    conn.getFields();
-    return conn;
-  },
-
-
-  /*
   array = db.query(qry, [param, param, ...])
 
   Return an array of rows if the query is of a type that can return rows.
