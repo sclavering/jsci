7c610151a6253c82606b9051b9c5e08c7f4e58c9
Stephen Clavering <stephen@clavering.me.uk>
2009-02-14 13:21:45 +0000

    remove JSEXT1/http (the .js and the dir)

--- global/JSEXT1/open/http.js
+++
@@ -1,9 +0,0 @@
-/*
-      stream = open("http://host:port/path/file")
-
-  Opens a connection to an http server by issuing a GET request.
- */
-
-function(uri) {
-  return $parent.http.get($parent.http.encodeURI(uri), undefined, {getStream: true}).stream;
-}
