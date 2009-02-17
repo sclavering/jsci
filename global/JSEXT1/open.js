944517c91c7215338f06329464ae8086f6337b43
Stephen Clavering <stephen@clavering.me.uk>
2009-02-17 00:52:37 +0000

    reimplement JSEXT1.open() to just call |new File| (thus dropping unused support for file:// URIs, leaving just filenames supported

--- global/JSEXT1/open.js
+++ global/JSEXT1/open.js
@@ -1,21 +1,10 @@
 /*
-    open(uri, [...])
+open(filename, mode)
 
-Opens and returns the resource identified by _uri_.
-If the _uri_ does not match a regexp for a URI, then
-the _uri_ parameter is interpreted as a filename.
+An alias for:  new File(filenmae mode)
 
-Note: file:// is the only supported URI protocol (support for http:// and others was removed).
+(Formerly dealt with URIs too.)
 */
-
-(function(curdir){
-  return function(uri) {
-    uri = url.parse(uri);
-    if(uri.protocol && uri.protocol != "file") throw Error("Unknown protocol handler '" + uri.protocol + "'");
-    return openfile.apply(openfile, arguments);
-  }
-
-  function openfile(uri, mode) {
-    return new JSEXT1.File(uri.fullPath, mode);
-  }
-})(this)
+function(filename, mode) {
+  return new JSEXT1.File(uri.fullPath, mode);
+}
