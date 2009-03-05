77342762644575543b394a10b1194dada478cafc
Stephen Clavering <stephen@clavering.me.uk>
2009-03-05 11:18:22 +0000

    remove global/JSEXT1/activate/sqlite.js (since SQLite has been removed)

--- global/JSEXT1/activate/sqlite.js
+++
@@ -1,8 +0,0 @@
-/*
-  Opens an SQLite database and returns the
-  corresponding [[$parent.Sqlite]] object.
-new JSX10.File() */
-
-function(name,extension) {
-  return new JSEXT1.Sqlite(this.$path+JSEXT_config.sep+name+extension);
-}
