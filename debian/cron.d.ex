5465bfc5618b3fc94f9abd8d1ba5d21824486c0e
Stephen Clavering <stephen@clavering.me.uk>
2009-03-06 18:13:30 +0000

    remove the debian/ dir, since it's unlikely i'll be packaging this version of jsext

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the jsext package
-#
-0 4	* * *	root	jsext_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(jsext):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="jsext" command="/usr/bin/jsext"
