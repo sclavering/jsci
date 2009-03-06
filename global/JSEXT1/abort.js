62f7d1820d9669b4f145c7d673281e4864966a18
Stephen Clavering <stephen@clavering.me.uk>
2009-03-06 19:50:16 +0000

    remove unused JSEXT1.abort()

--- global/JSEXT1/abort.js
+++
@@ -1,9 +0,0 @@
-  /*
-      abort()
-          
-Abort the interpreter immediately.  This 'dumps core' or otherwise fails
-in the hardest way possible on the hosting operating system.
-        */
-  
-  
-clib.abort;
