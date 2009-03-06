a2677530194eb4ff5d5997513823a30a658bacdf
Stephen Clavering <stephen@clavering.me.uk>
2009-03-06 19:49:32 +0000

    remove unused JSEXT1.getpid()

--- global/JSEXT1/getpid.js
+++
@@ -1,9 +0,0 @@
-  /*
-        num = getpid()
-
-    Returns the process ID of the current process.  (This is often used by routines that generate unique
-    temporary file names.)
-
-  */
-
-clib.getpid;
