232fe716477efdce897016535f991ab7de9839e1
Stephen Clavering <stephen@clavering.me.uk>
2009-03-07 06:47:43 +0000

    JSEXT.activate.js(): remove the special code for files called "with.js" (allowing control of the scope chain), since it's unused

--- global/JSEXT1/activate/js.js
+++ global/JSEXT1/activate/js.js
@@ -1,19 +1,6 @@
 /*
-
-  JavaScript files are interpreted with the current directory's
-  ActiveDirectory object on top of the scope chain.
-
- */
-
-function (name,extension) {
-  var w;
-  if (name!="with" && (w=this['with'])) {
-    var before="";
-    for (var i=w.length; i--;)
-      before+="with(this['with']["+i+"])";
-    before+="with(this){";
-    return load.call(this, this.$path + '/' + name + extension, before, "}");
-  } else {
-    return load.call(this, this.$path + '/' + name + extension);
-  }
+JavaScript files are interpreted with the current directory's ActiveDirectory object on top of the scope chain.
+*/
+function(name, extension) {
+  return load.call(this, this.$path + '/' + name + extension);
 }
