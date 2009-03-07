2b9666cb6cd48b7d599babe40da51ca23d5b98ba
Stephen Clavering <stephen@clavering.me.uk>
2009-03-07 06:48:36 +0000

    JSEXT1.shell(): stop passing JSEXT1.activate to ActiveDirecory, since it's the implicit default set of handlers anyway

--- global/JSEXT1/shell.js
+++ global/JSEXT1/shell.js
@@ -35,7 +35,7 @@
 function() {
 
   if(environment.HOME && JSEXT1.isdir(environment.HOME + '/.jsext')) {
-    var newglob = new JSEXT1.ActiveDirectory(environment.HOME + '/.jsext', JSEXT1.activate);
+    var newglob = new JSEXT1.ActiveDirectory(environment.HOME + '/.jsext');
     var oldglob=__proto__;
     __proto__=newglob;
     newglob.__proto__=oldglob;
