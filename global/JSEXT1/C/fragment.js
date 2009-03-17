b08bc2300b22d0c26e45c31e942f101df1720372
Stephen Clavering <stephen@clavering.me.uk>
2009-03-17 23:59:13 +0000

    JSEXT1/C/fragment.js: formatting fixes

--- global/JSEXT1/C/fragment.js
+++ global/JSEXT1/C/fragment.js
@@ -1,42 +1,37 @@
 /*
+obj = fragment (code, default_dl)
 
-      obj = fragment (code, default_dl)
+Examines a C program using [[$curdir.parse]] and returns an object containing all the symbols exported by the program.
 
-  Examines a C program using [[$curdir.parse]] and returns an
-  object containing all the symbols exported by the program.
+### Arguments ###
 
-  ### Arguments ###
+* _code_: An XML object as returned from [[$curdir.ctoxml]]
+* _default\_dl_: A Dl object where pointers can be resolved
 
-  * _code_: An XML object as returned from [[$curdir.ctoxml]]
-  * _default\_dl_: A Dl object where pointers can be resolved
+### Return value ###
 
-  ### Return value ###
+An object containing strings, which is JavaScript source code
+representing each symbol exported by the C program. Symbols
+are not exported if they generate code and can not be resolved
+in _default\_dl_ or any library mentioned in #pragma directives.
+*/
+function(code, default_dl) {
+  var parsed = parse(code, default_dl);
+  var src = {};
 
-  An object containing strings, which is JavaScript source code
-  representing each symbol exported by the C program. Symbols
-  are not exported if they generate code and can not be resolved
-  in _default\_dl_ or any library mentioned in #pragma directives.
+  for(var i in parsed.su)
+    if(parsed.expsym[i])
+      src[i] = parsed.su[i];
 
- */
-
-  function (code, default_dl) {
-    var parsed=parse(code, default_dl);
-    
-    var src={};
-    
-    for (var i in parsed.su)
-      if (parsed.expsym[i])
-	src[i]=parsed.su[i];
-    
-    for (var i in parsed.sym) {
-      if (parsed.expsym[i]) {
-	if (src[i]) {
-	  src[i] = "(this['" + i + "']=" + src[i] + "," + parsed.sym[i] + ")";
-	} else {
-	  src[i] = parsed.sym[i];
-	}
+  for(var i in parsed.sym) {
+    if(parsed.expsym[i]) {
+      if(src[i]) {
+        src[i] = "(this['" + i + "']=" + src[i] + "," + parsed.sym[i] + ")";
+      } else {
+        src[i] = parsed.sym[i];
       }
     }
-    
-    return src;
   }
+
+  return src;
+}
