57aa297a18e13de87177e6c94ea726e78fad8c6d
Stephen Clavering <stephen@clavering.me.uk>
2009-03-17 23:58:15 +0000

    JSEXT1/C/ctoxml.js: formatting fixes

--- global/JSEXT1/C/ctoxml.js
+++ global/JSEXT1/C/ctoxml.js
@@ -1,17 +1,20 @@
 /*
-
-    xml = ctoxml(code)
+xml = ctoxml(code)
 
 Runs a C program through a parser and returns an XML object
 containing the parse tree.
-
 */
-
 (function(){
+  const libctoxml = Dl($path + '/libctoxml.so');
 
-  var libctoxml = Dl($path + '/libctoxml.so');
-  var ctoxml=libctoxml.pointer('ctoxml',Type['function'](Type.pointer(Type.char),[{'const':false,name:'C',type:Type.pointer(Type.char)},{'const':false,name:'errorpos',type:Type.pointer(Type.int)}],false,'cdecl')).$;
-  var free=libctoxml.pointer('ctoxml_free',Type['function'](Type['void'],[{'const':false,name:'ptr',type:Type.pointer(Type['void'])}],false,'cdecl')).$;
+  const ctoxml = libctoxml.pointer('ctoxml', Type['function'](Type.pointer(Type.char), [
+      { 'const': false, name: 'C', type: Type.pointer(Type.char) },
+      { 'const': false, name: 'errorpos', type: Type.pointer(Type.int) }
+    ], false, 'cdecl')).$;
+
+  const free = libctoxml.pointer('ctoxml_free', Type['function'](Type['void'], [
+      { 'const': false, name: 'ptr', type: Type.pointer(Type['void']) }
+    ], false, 'cdecl')).$;
 
   return function (code) {
     var errorpos=[-1];
@@ -20,7 +23,7 @@ containing the parse tree.
       free(ret);
       throw("C.ctoxml: syntax error at "+errorpos);
     }
-    
+
     var ret2=new XML(ret.string());
     free(ret);
     return ret2;
