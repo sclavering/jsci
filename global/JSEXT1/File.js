52f1d2adc292f2866de835ed4f0c3ac54df64f20
Stephen Clavering <stephen@clavering.me.uk>
2009-03-18 03:23:00 +0000

    JSEXT1/File.js: fix code formatting

--- global/JSEXT1/File.js
+++ global/JSEXT1/File.js
@@ -1,97 +1,78 @@
-  /*
-        new File(filename [, mode])
-        new File(FILE *)
-    
-    Opens an existing file or creates a new one. The second form
-    of the constructor can be used when a library function returns
-    a FILE * which you want to encapsulate in a file object.
+/*
+new File(filename [, mode])
+new File(FILE *)
 
-    The file is closed when the file object is garbage collected,
-    or when [[$curdir.prototype.close]] is called, whichever comes
-    first.
+Opens an existing file or creates a new one. The second form of the constructor can be used when a library function returns a FILE * which you want to encapsulate in a file object.
 
-    ### Opening processes ###
+The file is closed when the file object is garbage collected, or when [[.close]] is called, whichever comes first.
 
-    Filenames that end in | are interpreted as commands that
-    are to be executed. The standard output of these commands
-    can be read through the newly created File object.
+### Opening processes ###
 
-        yes = new File("yes |");
+Filenames that end in | are interpreted as commands that are to be executed. The standard output of these commands can be read through the newly created File object.
 
-    Filenames that begin with | are interpreted as commands that
-    are to be executed. The standard input of these commands
-    can be witten through the newly created File object.
+yes = new File("yes |");
 
-        letter = new File("| mail");
+Filenames that begin with | are interpreted as commands that are to be executed. The standard input of these commands can be witten through the newly created File object.
 
-    To read and write to a process, use [[$curdir.pipe2]].
+letter = new File("| mail");
 
-  */
-  
-	function(filename, mode) {
-	    
-	  function popen(command) {
-	    if (mode!="r" && mode!="w" && mode!="a") throw("popen: Invalid mode");
-	    
-	    fp=clib.popen(command, mode);
-	    if (!fp)
-	      throw new Error('File: Coupld not open process \''+command+'\'');
-	      
-	    fp.finalize=clib.pclose;
+To read and write to a process, use [[$curdir.pipe2]].
+*/
 
-	    this.name="<command "+command+">";
-	    this.close=pclose;
-	    this.fp=fp;
-	  }
+function(filename, mode) {
 
-	  function pclose() {
-	    if (!this.closed) {
-	      var ret=clib.pclose(this.fp);
-	      this.fp.finalize=null;
-	      this.closed=true;
-	      if (ret>=256)
-		ret/=256;
-	      return ret;
-	    }
-	  }
+  function popen(command) {
+    if(mode != "r" && mode != "w" && mode != "a") throw("popen: Invalid mode");
+    fp = clib.popen(command, mode);
+    if(!fp) throw new Error("File: Coupld not open process '" + command + "'");
+    fp.finalize = clib.pclose;
+    this.name = "<command " + command + ">";
+    this.close = pclose;
+    this.fp = fp;
+  }
 
-	  var fp;
-	  if (arguments.length>0) {
-	    if (typeof filename == "string") {
-	      if (filename[0]=='|') {
-		if (filename[filename.length-1]=='|') {
-		  // | and |.
-		  throw new Error("Use popen2 for filters");
-		} else {
-		  // | only.
-		  if (mode===undefined)
-		    mode="w";
-		  popen.call(this,filename.substr(1));
-		}
-	      } else if (filename[filename.length-1]=='|') {
-		// only |
-		if (mode===undefined)
-		  mode="r";
-		popen.call(this,filename.substr(0,filename.length-1));
-	      } else {
-		// file
-		if (mode===undefined)
-		  mode="r";
-	      
-		fp=clib.fopen(filename,mode);
-		if (fp==null)
-		  throw new Error('File: Could not open file \''+filename+'\'');
-		fp.finalize=clib.fclose;
-		this.name=filename;
-		this.fp=fp;
-	      }
-	      
-	    } else {
-	      // filename is a pointer
-	      this.fp=filename;
-	      this.name="<unknown>";
-	    }
-	    
-	    this.closed=false;
-	  }
-	}
+  function pclose() {
+    if(!this.closed) {
+      var ret = clib.pclose(this.fp);
+      this.fp.finalize = null;
+      this.closed = true;
+      if(ret >= 256) ret /= 256;
+      return ret;
+    }
+  }
+
+  var fp;
+  if(arguments.length > 0) {
+    if(typeof filename == "string") {
+      if(filename[0] == '|') {
+        if(filename[filename.length - 1] == '|') {
+          // | and |.
+          throw new Error("Use popen2 for filters");
+        } else {
+          // | only.
+          if(mode === undefined) mode="w";
+          popen.call(this, filename.substr(1));
+        }
+      } else if(filename[filename.length - 1] == '|') {
+        // only |
+        if(mode === undefined) mode="r";
+        popen.call(this, filename.substr(0, filename.length - 1));
+      } else {
+        // file
+        if(mode === undefined) mode="r";
+        fp = clib.fopen(filename, mode);
+        if(fp == null) throw new Error("File: Could not open file '" + filename + "'");
+        fp.finalize = clib.fclose;
+        this.name = filename;
+        this.fp = fp;
+      }
+
+    } else {
+      // filename is a pointer
+      this.fp = filename;
+      this.name = "<unknown>";
+    }
+
+    this.closed = false;
+  }
+}
