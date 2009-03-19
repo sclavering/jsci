b8691cd8f0a5fd9518bb6f094737c410ee26bc2c
Stephen Clavering <stephen@clavering.me.uk>
2009-03-19 08:46:00 +0000

    JSEXT1/GC.c: remove JS_THREADSAFE, and minor code reformatting

--- global/JSEXT1/GC.c
+++ global/JSEXT1/GC.c
@@ -7,21 +7,13 @@
 #include <fcntl.h>
 
 static JSBool GC(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
-#ifdef JS_THREADSAFE
-  jsrefcount count=JS_SuspendRequest(cx);
-#endif
   JS_GC(cx);
-#ifdef JS_THREADSAFE
-  JS_ResumeRequest(cx, count);
-#endif
   return JS_TRUE;
 }
 
-JSBool
-JSX_init(JSContext *cx,  JSObject *obj, int argc, jsval *argv, jsval *rval) {
+JSBool JSX_init(JSContext *cx, JSObject *obj, int argc, jsval *argv, jsval *rval) {
   JSFunction *jsfun=JS_NewFunction(cx, GC, 0, 0, 0, 0);
-  if (!jsfun)
-    return JS_FALSE;
+  if(!jsfun) return JS_FALSE;
   *rval=OBJECT_TO_JSVAL(JS_GetFunctionObject(jsfun));
   return JS_TRUE;
 }
