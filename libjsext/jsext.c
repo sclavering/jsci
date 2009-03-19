a4a95d73cf643d779bc3947f08ecb16d15a01206
Stephen Clavering <stephen@clavering.me.uk>
2009-03-19 23:42:29 +0000

    jsext.c: convert comments to utf8

--- libjsext/jsext.c
+++ libjsext/jsext.c
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2006, Berges Allmenndigitale Rådgivningstjeneste
+* Copyright (c) 2006, Berges Allmenndigitale RÃ¥dgivningstjeneste
 * All rights reserved.
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are met:
@@ -9,7 +9,7 @@
 *     * Redistributions in binary form must reproduce the above copyright
 *       notice, this list of conditions and the following disclaimer in the
 *       documentation and/or other materials provided with the distribution.
-*     * Neither the name of Berges Allmenndigitale Rådgivningstjeneste nor the
+*     * Neither the name of Berges Allmenndigitale RÃ¥dgivningstjeneste nor the
 *       names of its contributors may be used to endorse or promote products
 *       derived from this software without specific prior written permission.
 *
