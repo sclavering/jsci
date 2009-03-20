210f5bc74e66b1fb71007d8c4d7e22a00411cc74
Stephen Clavering <stephen@clavering.me.uk>
2009-03-20 20:07:52 +0000

    ctoxml: remove dead code in strbuf.c

--- global/JSEXT1/C/0-ctoxml/strbuf.c
+++ global/JSEXT1/C/0-ctoxml/strbuf.c
@@ -16,20 +16,6 @@ void strbuf_free(struct strbuf *buf) {
   free(buf);
 }
 
-/*
-void strbuf_cpy(struct strbuf *buf, char *str) {
-  int len=strlen(str);
-  if (len+1>buf->capacity) {
-    buf->capacity=len+1;
-    buf->buf=realloc(buf->buf, buf->capacity);
-    if (!buf->buf) exit(1);
-  }
-  buf->ptr=buf->buf;
-  memcpy(buf->buf, str, len+1);
-  buf->len=len;
-}
-*/
-
 void strbuf_ncpy(struct strbuf *buf, char *str, int n) {
   if (n+1>buf->capacity) {
     buf->capacity=n+1;
@@ -49,22 +35,11 @@ void strbuf_clear(struct strbuf *buf) {
   buf->len=0;
 }
 
-/*
-void strbuf_catchar(struct strbuf *buf, char c) {
-  if (buf->len+1 < buf->capacity) {
-    buf->buf[buf->len++]=c;
-    buf->buf[buf->len]=0;
-  } else {
-    char str[2]={c,0};
-    strbuf_cat(buf,str);
-  }
-}
-*/
-
 void strbuf_cat(struct strbuf *buf, char *str) {
   int len=strlen(str);
   while (len+buf->len+1 > buf->capacity) {
     int offset=buf->ptr-buf->buf;
+
     buf->capacity*=2;
     buf->buf=realloc(buf->buf, buf->capacity);
     if (!buf->buf) exit(1);
@@ -81,6 +56,7 @@ void strbuf_ncat(struct strbuf *buf, char *str, int n) {
   if (len>n) len=n;
   while (len+buf->len+1 > buf->capacity) {
     int offset=buf->ptr-buf->buf;
+
     buf->capacity*=2;
     buf->buf=realloc(buf->buf, buf->capacity);
     if (!buf->buf) exit(1);
