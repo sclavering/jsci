08daf0d30401435582735049fbf39288c406f42a
Stephen Clavering <stephen@clavering.me.uk>
2009-05-10 18:41:03 +0000

    fcgi: remove .readline() .readlines() and .writelines() from the internal File object used to replace stdin/stdout/stderr

--- global/JSEXT1/fcgi.js
+++ global/JSEXT1/fcgi.js
@@ -125,57 +125,6 @@ File.prototype = {
     return buf.string(len);
   },
 
-
-  // Not binary-safe
-  /*
-  stream.readline([n])
-  
-  Reads up to n consecutive bytes from the input stream
-  into the character array str.  Stops before n bytes
-  have been read if '\n' or EOF is read.  The terminating '\n'
-  is copied to str.
-  
-  If no argument is given, reads until '\n' or EOF is read
-  
-  **Not binary-safe**: If the input stream contains \0 characters,
-  they will cause reading to terminate.
-
-  ### Results ###
-
-  A [[String]] containing the bytes read.  If the length is smaller than n,
-  the end of input has been reached.
-  */
-  readline: function(size) {
-    if (arguments.length<1) size=-1;
-    if (size<0) {
-      var trysize=512;
-      var ret="";
-      while (!this.eof()) {
-        ret+=this.readline(trysize);
-        if (ret[ret.length-1]=='\n') break;
-        trysize*=2;
-      }
-      return ret;
-    }
-    var buf=Pointer.malloc(size+1);
-    //      var beforepos=this.tell();
-    const line = libfcgi.FCGX_GetLine(buf, size + 1, this.stream);
-    if(!line) return "";
-    return buf.string();
-  },
-
-  /*
-  array = stream.readlines([size])
-  
-  Reads up to [size] bytes. If no argument is given, reads until
-  EOF. Splits result into lines an returns an array of strings
-  which do not contain the '\n' character.
-  */
-  readlines: function(size) {
-    var buf=this.read(size);
-    return buf.split(/\n/);
-  },
-
   /*
   stream.write(str)
   
@@ -193,16 +142,6 @@ File.prototype = {
     const ret = libfcgi.FCGX_PutStr(str, str.length, this.stream);
     if(ret == -1) throw new Error("fcgi: write");
     return ret;
-  },
-
-  /*
-  stream.writelines(array)
-  
-  Writes each of the strings in the array. Adds '\n'
-  characters.
-  */
-  writelines: function(obj) {
-    for (i in obj) this.write(obj[i]+"\n");
   },
 }
 
