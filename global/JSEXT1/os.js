3f5cde4cc7ca263b0eebfa9d532cf07cb7ef4c85
Stephen Clavering <stephen@clavering.me.uk>
2009-05-11 01:41:25 +0000

    JSEXt1/os.js: remove unused .confstr(.) and .uname()

--- global/JSEXT1/os.js
+++ global/JSEXT1/os.js
@@ -35,43 +35,4 @@ the description given in string.
   errno: function() {
     return clib.__errno_location().$;
   },
-
-  /*
-          string = os.confstr(num)
-
-      Return a string-valued system configuration variable.
-     */
-
-
-  confstr: function(num) {
-    var trylen=256;
-    var buf=Pointer.malloc(trylen);
-    for (;;) {
-      var len=clib.confstr(num, buf, trylen);
-      if (len<trylen) break;
-      trylen*=2;
-      buf.realloc(trylen);
-    }
-    return buf.string(len);
-  },
-
-  /*
-          uname() -> (sysname, nodename, release, version, machine)
-
-      Return an object identifying the current operating system.
-     */
-
-  uname: function() {
-    var buf=Pointer(clib['struct utsname']);
-    if (clib.uname(buf)) {
-      throw new Error(os.error("uname"));
-    }
-    return {sysname: buf.member(0,"sysname").string(),
-	    nodename: buf.member(0,"nodename").string(),
-	    release: buf.member(0,"release").string(),
-	    version: buf.member(0,"version").string(),
-	    machine: buf.member(0,"machine").string()};
-	    
-  }
-
 })
