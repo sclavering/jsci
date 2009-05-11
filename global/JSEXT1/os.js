daaaf19a99f12897e52ee5602506917783b78c6b
Stephen Clavering <stephen@clavering.me.uk>
2009-05-11 01:43:44 +0000

    clean up JSEXT1/os.js

--- global/JSEXT1/os.js
+++ global/JSEXT1/os.js
@@ -1,37 +1,32 @@
 /*
-
-  Contains a few os-related functions
-
- */
-
+Miscellaneous wrappers for os-related things in clib where the raw ffi interface is too horrible
+*/
 ({
   /*
-    string = os.strerror(errnum)
+  string = os.strerror(errnum)
 
-Returns error text for a given error number
-     */
-
+  Returns error text for a given error number
+  */
   strerror: function(errnum) {
     return clib.strerror(errnum).string();
   },
 
+
   /*
-    string = os.error(string2)
+  string = os.error(string2)
 
-Returns a string with the last error and
-the description given in string.
-*/
-
+  Returns a string with the last error and the description given in string.
+  */
   error: function(str) {
-    return str+": "+os.strerror(os.errno());
+    return str + ": " + os.strerror(os.errno());
   },
 
+
   /*
-        num = os.errno()
+  num = os.errno()
 
-    Returns last error number
+  Returns last error number
   */
-
   errno: function() {
     return clib.__errno_location().$;
   },
