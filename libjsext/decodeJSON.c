ccd6dda56859eb274bb609b999f88527a96030e2
Stephen Clavering <stephen@clavering.me.uk>
2009-06-03 13:42:17 +0000

    decodeJSON.c: remove the local copy of JSX_ReportException()

--- libjsext/decodeJSON.c
+++ libjsext/decodeJSON.c
@@ -3,6 +3,7 @@
 #include <jsobj.h>
 #include <jsfun.h>
 #include <stdarg.h>
+#include "util.h"
 
 struct JSON {
   jschar *p;
@@ -10,27 +11,6 @@ struct JSON {
   JSContext *cx;
   jsval *vp;
 };
-
-static JSBool JSX_ReportException(JSContext *cx, char *format, ...) {
-  int len;
-  char *msg;
-  JSString *Str;
-  va_list va;
-  jsval str;
-
-  va_start(va, format);
-  msg=JS_malloc(cx, 81);
-  va_start(va, format);
-  len=vsnprintf(msg,80,format,va);
-  msg[80]=0;
-
-  va_end(va);
-  Str=JS_NewString(cx, msg, len);
-  str=STRING_TO_JSVAL(Str);
-  JS_SetPendingException(cx, str);
-
-  return JS_FALSE;
-}
 
 static JSBool syntaxerror(struct JSON *s) {
   JSX_ReportException(s->cx, "JSON: Syntax error at offset %d",s->p-s->start);
