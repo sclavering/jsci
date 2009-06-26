4cedc184ce12108745981dfc522b90056e58abbb
Stephen Clavering <stephen@clavering.me.uk>
2009-06-26 23:05:21 +0000

    load.c: simplify by using JS_EvaluateScript() rather than JS_CompileScript() and JS_ExecuteScript()

--- libjsext/load.c
+++ libjsext/load.c
@@ -20,53 +20,31 @@ The file may start with a #! line, which is ignored.
 
 
 static JSBool load(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
-  JSScript *script=0;
-  JSObject *scrobj=0;
-
   if(!JSVAL_IS_STRING(argv[0])) return JSX_ReportException(cx, "load(): first argument must be a string");
-
   char *filename = JS_GetStringBytes(JSVAL_TO_STRING(argv[0]));
-
   int fd = open(filename, O_RDONLY);
   if(fd == -1) return JSX_ReportException(cx, "load(): could not open %s",filename);
 
+  JSBool ok = JS_FALSE;
   struct stat S;
   fstat(fd, &S);
   char *buf = malloc(S.st_size);
   if (!buf) {
     JSX_ReportException(cx, "load: Out of memory loading %s", filename);
-    goto failure;
+    goto exit;
   }
   if(read(fd, buf, S.st_size) != S.st_size) {
     JSX_ReportException(cx, "load: Error loading %s",filename);
-    goto failure;
+    goto exit;
   }
-
-  script=JS_CompileScript(cx, obj, buf, S.st_size, filename, 1);
-  if (!script) goto failure;
-  scrobj=JS_NewScriptObject(cx, script);
-  
-  free(buf);
-  buf=0;
-  
   *rval=JSVAL_ZERO;
+  if(!JS_EvaluateScript(cx, obj, buf, S.st_size, filename, 1, rval)) goto exit;
 
-  close(fd);
-  fd=0;
-  
-  if (!JS_ExecuteScript(cx, obj, script, rval))
-    goto failure;
-
-  JS_RemoveRoot(cx, &scrobj);
-
-  return JS_TRUE;
-
- failure:
-  if (buf) free(buf);
-  if (fd) close(fd);
-  if (scrobj)
-    JS_RemoveRoot(cx, &scrobj);
-  return JS_FALSE;
+  ok = JS_TRUE;
+ exit:
+  if(buf) free(buf);
+  if(fd) close(fd);
+  return ok;
 }
 
 
