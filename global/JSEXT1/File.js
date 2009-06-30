ef2416ecc28668d8671aff42116ef8f14942a85e
Stephen Clavering <stephen@clavering.me.uk>
2009-06-30 17:25:20 +0000

    File: remove unused instance methods .seekEnd() and .seekRel()

--- global/JSEXT1/File.js
+++ global/JSEXT1/File.js
@@ -203,29 +203,7 @@ File.prototype = {
     clib.clearerr(this.fp);
     clib.fseek(this.fp, offset, clib.SEEK_SET);
   },
-  
 
-  /*
-  file.seekEnd (offset)
-    
-  Moves file pointer to the given position, relative to the end of the file. _offset_ will generally be zero or a negative number.
-  */
-  seekEnd: function(offset) {
-    clib.clearerr(this.fp);
-    clib.fseek(this.fp, offset, clib.SEEK_END);
-  },
-  
-
-  /*
-  file.seekRel (offset)
-    
-  Moves file pointer to the given position, relative to the current position.
-  */
-  seekRel: function(offset) {
-    clib.clearerr(this.fp);
-    clib.fseek(this.fp, offset, clib.SEEK_CUR);
-  },
-  
 
   /*
   file.stat() -> obj
